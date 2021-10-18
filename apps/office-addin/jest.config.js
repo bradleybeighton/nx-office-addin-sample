2f12bf140691e71a7b7f304da34b24dcee4dffe6
Bradley Beighton <brad.beighton@gmail.com>
2021-10-18 16:41:19 +0100

    initial commit

---
+++ apps/office-addin/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  displayName: 'office-addin',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  coverageDirectory: '../../coverage/apps/office-addin',
+  transform: {
+    '^.+\\.(ts|js|html)$': 'jest-preset-angular',
+  },
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
